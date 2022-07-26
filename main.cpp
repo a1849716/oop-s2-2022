e2a137d6283585221166543968e6e83daf749c7c
Richie Xiao <richiexiao0@gmail.com>
2022-07-26 10:37:01 +0930

    removed count function in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,6 @@ using namespace std;
 // but will be implemented somewhere else
 extern int count(int[], int);
 
-int count(int array[], int n){
-  int count = 0 ;
-  for (int i = 0; i < n; i++){
-    if (array[i] % 2 == 0){
-      count++;
-    }
-  }
-  return count;
-}
-
 int main() {
     int array[5] = {4,5,6,7,8};
     cout << "The number is: " << count(array, 5) << std::endl;

97c6d4826a172d936ced733224bc7cefccbf4a1c
Richie Xiao <a1849716@adelaide.edu.au>
2022-07-28 01:04:40 +0930

    small bug fixes on function-1-3 & function-2-3

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,12 +1,11 @@
 //function to add all integers in array
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
-extern int int_count(int[], int, int);
+extern int num_count(int[], int, int);
 
 int main(){
   int array[6] = {2,3,6,8,2,1};
-  cout << "The number "<< 2 <<" is repeated "<< int_count(array, 6, 2) << " times."<<endl;
+  cout << "The number "<< 2 <<" is repeated "<< num_count(array, 6, 2) << " times."<<endl;
   return 0;
 }

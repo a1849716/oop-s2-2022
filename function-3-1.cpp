3dc4e93121de3f466f1291004bf9a7a9b43045f3
Richie Xiao <richiexiao0@gmail.com>
2022-07-28 13:05:58 +0930

    lamoxd

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -6,8 +6,7 @@ bool is_fanarray(int array[], int n) {
   if (n < 1) {
     fan_arr = false;
   }
-  std::cout << n/2<< std::endl;
-  
+
   for (int i = 0; i < n / 2; i++) {
     for (int j = n / 2; j > -1; j--) {
       if (array[i] != array[j]) {

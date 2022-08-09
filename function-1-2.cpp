e7a03b1cede9a96cd6b8b1e0e9ebdd97b9ddbea1
Richie Xiao <richiexiao0@gmail.com>
2022-08-09 10:30:37 +0930

    I REALLY WANT TO LEAVE now

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -6,12 +6,13 @@ void print_class(std::string courses[4], std::string students[5],
                  int report_card[5][4], int nstudents) {
   cout << "Report Card ";
   for (int i = 0; i < 4; i++) {
-    cout << courses[i] << " ";
+    cout << courses[i];
     if (i != 3) {
       cout << " ";
     }
   }
   cout << endl;
+
   for (int r = 0; r < nstudents; r++) {
     cout << students[r] << " ";
     for (int c = 0; c < 4; c++) {

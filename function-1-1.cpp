18a8d72a878205808faa2763a1aa5c4415d9fed9
Richie Xiao <richiexiao0@gmail.com>
2022-08-09 10:25:12 +0930

    PLEASE STOP

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,8 +4,11 @@ using namespace std;
 void printer(int array[10][10]) {
   for (int r = 0; r < 10; r++) {
     for (int c = 0; c < 10; c++) {
-      cout << array[r][c] << " ";
+      cout << array[r][c];
+      if (c != 9) {
+        cout << " ";
+      }
+      cout << endl;
     }
-    cout << endl;
   }
 }
--- function-1-2.cpp
+++ function-1-2.cpp
@@ -6,12 +6,17 @@ void print_class(std::string courses[4], std::string students[5],int report_card
   cout << "Report Card ";
   for (int i = 0; i < 4; i++) {
     cout << courses[i] << " ";
+    if (i != 3){
+    cout << " ";
   }
   cout << endl;
   for (int r = 0; r < nstudents; r++) {
     cout << students[r] << " ";
     for (int c = 0; c < 4; c++) {
-      cout << report_card[r][c] << " ";
+      cout << report_card[r][c];
+    }
+    if (c != 3){
+      cout << " ";
     }
     cout << endl;
   }

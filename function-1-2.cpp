687cc2b190e7b83898d5a151ab0cc0ced3f04780
Richie Xiao <richiexiao0@gmail.com>
2022-08-09 10:29:13 +0930

    I REALLY WANT TO LEAVE

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,22 +2,24 @@
 #include <string>
 using namespace std;
 
-void print_class(std::string courses[4], std::string students[5],int report_card[5][4], int nstudents) {
+void print_class(std::string courses[4], std::string students[5],
+                 int report_card[5][4], int nstudents) {
   cout << "Report Card ";
   for (int i = 0; i < 4; i++) {
     cout << courses[i] << " ";
-    if (i != 3){
-    cout << " ";
+    if (i != 3) {
+      cout << " ";
+    }
   }
   cout << endl;
   for (int r = 0; r < nstudents; r++) {
     cout << students[r] << " ";
     for (int c = 0; c < 4; c++) {
       cout << report_card[r][c];
+      if (c != 3) {
+        cout << " ";
+      }
     }
-    if (c != 3){
-      cout << " ";
-    }
-    cout << endl;
+  cout << endl;
   }
 }

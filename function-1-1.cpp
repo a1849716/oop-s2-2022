f0b522dfc401051d17d6faa81d4208399f0a6ccc
Richie Xiao <richiexiao0@gmail.com>
2022-08-10 13:39:57 +0930

    second commit

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -5,7 +5,7 @@ void print_matrix(int array[10][10]) {
   for (int r = 0; r < 10; r++) {
     for (int c = 0; c < 10; c++) {
       cout << array[r][c];
-      if (c != 10){
+      if (c != 9){
         cout << " ";
       }
     }

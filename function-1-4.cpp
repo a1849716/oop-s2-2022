cca68b7c483bfb26e7c4953f02017d23651b90c9
Richie Xiao <richiexiao0@gmail.com>
2022-08-15 13:18:30 +0930

    lmaoxd

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 void copy_doubles(double *old_array,double *new_array,int length) {
-  for (int r = 0; r < length; r++){
+  *new_array = *new double(length);
+
+  for (int r = 0; r < length; r++) {
     *new_array = *(old_array+r);
-    new_array = new_array+r;
+    *new_array = *(new_array+r);
   }
 }

6d316a182334495e725b9a1e9003928eba48f75c
Richie Xiao <richiexiao0@gmail.com>
2022-08-15 13:15:54 +0930

    lmaoxd

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 void copy_integers(int old_array[], int new_array[], int length) {
-  int* old_ptr;
+  int* ptr;
   ptr = &old_array[0];
   for (int r = 0; r < length; r++) {
     new_array[r] = *(ptr+r);
   }
-  cout << endl;
 }
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 void copy_doubles(double *old_array,double *new_array,int length) {
-  for (int r = 0; r < length; r++) {
+  for (int r = 0; r < length; r++){
     *new_array = *(old_array+r);
     new_array = new_array+r;
   }
-
-  cout << endl;
 }

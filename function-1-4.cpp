60b5efdfddaf63b9f3fd668360a8b9bbfb827e34
Richie Xiao <richiexiao0@gmail.com>
2022-08-15 12:48:13 +0930

    part 1 1st try

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -6,9 +6,10 @@ void copy_doubles(double *old_array,double *new_array,int length) {
 
   for (int r = 0; r < length; r++) {
     *new_array = *(old_array+r);
-    cout << *new_array << " "<< new_array << endl;
-    cout << *(old_array+r) << " " << (old_array + r)<< endl;
-    cout << endl;
+    cout << *new_array;
+    if (r != length - 1){
+      cout << " ";
+    }
     *new_array = *(new_array+r);
   }
 

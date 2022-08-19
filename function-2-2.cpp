8136426c573719d293361489c91a2dfe745893c7
Richie Xiao <richiexiao0@gmail.com>
2022-08-19 16:55:30 +0930

    lamoxd

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,19 +1,15 @@
-#include<iostream>
-#include<climits>
+#include <climits>
+#include <iostream>
 using namespace std;
-  
-int maxSubArraySum(int a[], int size)
-{
-    int curr_max = INT_MIN, max_from_here = 0;
-  
-    for (int i = 0; i < size; i++)
-    {
-        max_ending_here = max_from_here + a[i];
-        if (curr_max < max_from_here)
-            curr_max = max_from_here;
-  
-        if (max_from_here < 0)
-            max_from_here = 0;
-    }
-    return curr_max;
+
+int max_sub_sum(int *nums, int length) {
+  int curr_max = INT_MIN, max_from_here = 0;
+
+  for (int i = 0; i < length; i++) {
+    max_from_here = max_from_here + nums[i];
+    if (curr_max < max_from_here) curr_max = max_from_here;
+
+    if (max_from_here < 0) max_from_here = 0;
+  }
+  return curr_max;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 
+extern int max_sub_sum(int *nums,int length);
+
 int main()
 {
     int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int n = sizeof(a)/sizeof(a[0]);
-    int max_sum = maxSubArraySum(a, n);
+    int max_sum = max_sub_sum(a, n);
     cout << "Maximum contiguous sum is " << max_sum;
     return 0;
 }

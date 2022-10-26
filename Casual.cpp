e410d8e7e9941cfb5666952f3597c095424b0821
Yuqi Xiao <a1849716@adelaide.edu.au>
2022-10-26 14:43:07 +1030

    part 3 not done

--- Casual.cpp
+++ Casual.cpp
@@ -32,16 +32,18 @@ void Casual::work(int mins){
   if (energyLevel > 0){
     energyLevel -= mins * 0.5;
   }
-  if (mins > 200){
+  if (mins > 200 || energyLevel < 0){
+    energyLevel = 0.0;
+  }
+  if (energyLevel < 0){
     energyLevel = 0.0;
   }
   if (dayCount < 4){
-    hoursWorked[dayCount] = mins/30;
+    hoursWorked[dayCount] = (mins/60) * 2;
     }
   else{
     hoursWorked[dayCount] = mins/60;
   }
-  hoursWorked[dayCount] += mins/60;
 };
 
 float Casual::pay(){
